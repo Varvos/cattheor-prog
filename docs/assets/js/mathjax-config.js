9d0d853968c9079d4ffca305d7fb77c527749332
Varvos <vartanvos@gmail.com>
2026-06-02 00:45:30 +0100

    fix: clean up JS and CSS, use CSS variable for random header colour

--- docs/assets/js/mathjax-config.js
+++ docs/assets/js/mathjax-config.js
@@ -1,26 +1,11 @@
-
 window.MathJax = {
     tex: {
-      inlineMath: [['$', '$'], ['\\(', '\\)']],
-      displayMath: [['$$', '$$'], ['\\[', '\\]']]
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']]
     },
     options: {
-      skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
-      ignoreHtmlClass: 'tex2jax_ignore',
-      processHtmlClass: 'tex2jax_process'
+        skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+        ignoreHtmlClass: 'tex2jax_ignore',
+        processHtmlClass: 'tex2jax_process'
     }
-  };
-  
-
-document.addEventListener("DOMContentLoaded", function() {
-    renderMathInElement(document.body, {
-        delimiters: [
-            {left: "$$", right: "$$", display: true},
-            {left: "\\[", right: "\\]", display: true},
-            {left: "$", right: "$", display: false},
-            {left: "\\(", right: "\\)", display: false}
-        ]
-    });
-});
-
-  
+};
--- docs/assets/js/random-color.js
+++ docs/assets/js/random-color.js
@@ -1,31 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  function getRandomColor() {
-    // Define a set of suitable colors that aren't too light
     var colors = [
-      '#028063', 
-      // '#778a9d', 
-      // '#345B63', 
-      // '#152D35', 
-      '#112A46',
-      '#4985c0',
-      '#377b40',
-      '#9b411b',
+        '#028063',
+        '#112A46',
+        '#4985c0',
+        '#377b40',
+        '#9b411b',
     ];
-    // Select a random color from the array
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-  var randomColor = getRandomColor();  // Store the random color in a variable
-
-  // Apply to .md-header
-  var headers = document.querySelectorAll('.md-header');
-  headers.forEach(function(header) {
-    header.style.backgroundColor = randomColor;
-  });
-
-  // Apply to tabbed labels
-  var labels = document.querySelectorAll('div.tabbed-set div.tabbed-labels label');
-  labels.forEach(function(label) {
-    label.style.backgroundColor = randomColor;
-  });
+    // Set as a CSS variable so hover/active states in CSS still work
+    document.documentElement.style.setProperty('--header-color', randomColor);
 });
